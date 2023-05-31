6cb027f2d978af73971dfcd694ad582f8ad286b5
Mohit <r.mohit0199@gmail.com>
2023-05-31 11:11:23 +0530

    Updated

--- App.js
+++ App.js
@@ -13,7 +13,34 @@ const savedNews = [];
 const handleSavedNews = (savedItem) => {
   savedNews.push(savedItem);
   console.log(savedNews);
-  alert("News saved")
+  alert("News Saved");
+}
+
+const displaySavedNews = () => {
+  newsContainer.innerHTML = "";
+  if (!savedNews) {
+    return;
+  }
+  savedNews.forEach((newsItem, index) => {
+    const div = document.createElement("div");
+    div.classList.add("newsItem");
+    div.innerHTML = `
+      <h2>${newsItem.title}</h2>
+      <p>${newsItem.content}</p>
+    `;
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "Delete";
+    deleteButton.onclick = function () {
+      deleteSavedNews(index);
+    };
+    div.appendChild(deleteButton);
+    newsContainer.appendChild(div);
+  });
+};
+
+const deleteSavedNews = (index) => {
+  savedNews.splice(index, 1);
+  displaySavedNews();
 }
 
 const getNews = (category = "All") => {
@@ -31,15 +58,15 @@ const getNews = (category = "All") => {
           <img src="${newsItem.imageUrl}" alt="${newsItem.title}">
           <p>${newsItem.content} <a href="${newsItem.readMoreUrl}">Read more</a></p>
           <div class="like-section" style="display: flex; align-items: center;">
-  <button class="like-button" style="font-size: 2.5em; background-color: transparent; border: none; cursor: pointer;" data-likes="0">&#9825;</button>
-  <span class="like-count" style="margin-left: 10px; font-weight: bold;">0</span>
-</div>
-<br>
-
+            <button class="like-button" style="font-size: 2.5em; background-color: transparent; border: none; cursor: pointer;" data-likes="0">&#9825;</button>
+            <span class="like-count" style="margin-left: 10px; font-weight: bold;">0</span>
+          </div>
+          <br>
         `;
         const button = document.createElement("button");
         button.innerHTML = "Save"
-        button.onclick = function () {
+        button.onclick = function (event) {
+          event.stopPropagation(); // Prevent event bubbling to parent elements
           handleSavedNews(newsItem)
         }
         div.appendChild(button);
@@ -54,8 +81,14 @@ const getNews = (category = "All") => {
           // Get the current number of likes
           let likes = parseInt(likeButton.dataset.likes);
 
-          // Increment the number of likes
-          likes++;
+          // Toggle the like button state and update the count accordingly
+          if (likeButton.classList.contains('liked')) {
+            likes--;
+            likeButton.classList.remove('liked');
+          } else {
+            likes++;
+            likeButton.classList.add('liked');
+          }
 
           // Update the like button and count
           likeButton.dataset.likes = likes;
@@ -66,20 +99,7 @@ const getNews = (category = "All") => {
 };
 
 const loadSavedNews = () => {
-  console.log("Saved News", savedNews)
-  newsContainer.innerHTML = "";
-  if (!savedNews) {
-    return;
-  }
-  savedNews.forEach((newsItem) => {
-    const div = document.createElement("div");
-    div.classList.add("newsItem");
-    div.innerHTML = `
-      <h2>${newsItem.title}</h2>
-      <p>${newsItem.content}</p>
-    `;
-    newsContainer.appendChild(div);
-  });
+  displaySavedNews();
 };
 
 loadSavedButton.addEventListener("click", loadSavedNews);
