817e911685c9f1f55f8230fbc69df3ccc6b35758
Mohit <r.mohit0199@gmail.com>
2023-06-08 12:27:07 +0530

    Updated API

--- App.js
+++ App.js
@@ -1,108 +1,129 @@
 const newsContainer = document.querySelector("#newsContainer");
-const saveButton = document.querySelector("#saveButton");
 const loadSavedButton = document.querySelector("#loadSavedButton");
 const loadNewsButton = document.querySelector("#loadNewsButton");
 const categorySelect = document.querySelector("#categorySelect");
 
 function loadNews() {
-  document.querySelector('#hidden').style.display = "block";
+  document.querySelector("#hidden").style.display = "block";
 }
 
 const savedNews = [];
 
-const handleSavedNews = (savedItem) => {
-  savedNews.push(savedItem);
-  console.log(savedNews);
-  alert("News Saved");
-}
-
+// Display Sved News 
 const displaySavedNews = () => {
   newsContainer.innerHTML = "";
   if (!savedNews) {
     return;
   }
   savedNews.forEach((newsItem, index) => {
-    const div = document.createElement("div");
-    div.classList.add("newsItem");
-    div.innerHTML = `
-      <h2>${newsItem.title}</h2>
-      <p>${newsItem.content}</p>
-    `;
-    const deleteButton = document.createElement("button");
-    deleteButton.innerHTML = "Delete";
-    deleteButton.onclick = function () {
-      deleteSavedNews(index);
-    };
+    const div = createNewsItemElement(
+      newsItem.title,
+      newsItem.description,
+      newsItem.articleUrl
+    );
+    const deleteButton = createDeleteButton(index);
     div.appendChild(deleteButton);
     newsContainer.appendChild(div);
   });
 };
 
+const createNewsItemElement = (title, description, imageUrl, articleUrl) => {
+  const div = document.createElement("div");
+  div.classList.add("newsItem");
+  div.innerHTML = `
+    <h2>${title}</h2>
+    <img src="${imageUrl}" alt="${title}">
+    <p>${description}</p>
+    <a href="${articleUrl}" target="_blank">Read more</a>
+  `;
+  const readMoreLink = div.querySelector("a");
+  readMoreLink.addEventListener("click", (event) => {
+    event.preventDefault();
+    window.location.href = articleUrl;
+  });
+  return div;
+};
+
+
+// Delete button in saved news section
+const createDeleteButton = (index) => {
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "Delete";
+  deleteButton.className = "delete-button"
+  deleteButton.onclick = function () {
+    deleteSavedNews(index);
+  };
+  return deleteButton;
+};
+
 const deleteSavedNews = (index) => {
   savedNews.splice(index, 1);
   displaySavedNews();
-}
+};
 
-const getNews = (category = "All") => {
+
+// get news from API
+const getNews = (category = "all") => {
+  const apiKey = '818c4c1d1bd34d11a54b8d27eba1b044'
   newsContainer.innerHTML = "";
-  fetch(`https://inshorts.deta.dev/news?category=${category}`)
+
+  let url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${apiKey}`;
+
+  fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      console.log("Data", data)
-      data.data.forEach((newsItem) => {
-        const div = document.createElement("div");
-        div.classList.add("newsItem");
-        div.innerHTML = `
-          <p>By <strong>${newsItem.author}</strong></p>
-          <h2>${newsItem.title}</h2>
-          <img src="${newsItem.imageUrl}" alt="${newsItem.title}">
-          <p>${newsItem.content} <a href="${newsItem.readMoreUrl}">Read more</a></p>
-          <div class="like-section" style="display: flex; align-items: center;">
-            <button class="like-button" style="font-size: 2.5em; background-color: transparent; border: none; cursor: pointer;" data-likes="0">&#9825;</button>
-            <span class="like-count" style="margin-left: 10px; font-weight: bold;">0</span>
-          </div>
-          <br>
-        `;
-        const button = document.createElement("button");
-        button.innerHTML = "Save"
-        button.onclick = function (event) {
-          event.stopPropagation(); // Prevent event bubbling to parent elements
-          handleSavedNews(newsItem)
-        }
-        div.appendChild(button);
+      console.log("Data", data);
+      data.articles.forEach((newsItem) => {
+        const div = createNewsItemElement(
+          newsItem.title,
+          newsItem.description,
+          newsItem.urlToImage,
+          newsItem.url
+        );
+        const saveButton = createSaveButton(newsItem);
+
+        div.appendChild(saveButton);
         newsContainer.appendChild(div);
-
-        // Get the like button and like count elements
-        const likeButton = div.querySelector('.like-button');
-        const likeCount = div.querySelector('.like-count');
-
-        // Add an event listener to the like button
-        likeButton.addEventListener('click', () => {
-          // Get the current number of likes
-          let likes = parseInt(likeButton.dataset.likes);
-
-          // Toggle the like button state and update the count accordingly
-          if (likeButton.classList.contains('liked')) {
-            likes--;
-            likeButton.classList.remove('liked');
-          } else {
-            likes++;
-            likeButton.classList.add('liked');
-          }
-
-          // Update the like button and count
-          likeButton.dataset.likes = likes;
-          likeCount.textContent = likes;
-        });
       });
     });
 };
+
+// save button in each news artices which display pop up when clicked
+const createSaveButton = (newsItem) => {
+  const saveButton = document.createElement("button");
+  saveButton.innerHTML = "Save";
+  saveButton.className = "save-button"
+  saveButton.onclick = function (event) {
+    event.stopPropagation();
+    handleSavedNews(newsItem);
+    showPopup("News saved");
+  };
+  return saveButton;
+};
+
+const handleSavedNews = (savedItem) => {
+  savedNews.push(savedItem);
+  console.log(savedNews);
+};
+
+const showPopup = (message) => {
+  const popup = document.getElementById("popup");
+  const popupMessage = document.getElementById("popup-message");
+  popupMessage.textContent = message;
+  popup.style.display = "flex";
+  setTimeout(() => {
+    popup.style.display = "none";
+  }, 2000);
+};
+
 
 const loadSavedNews = () => {
   displaySavedNews();
 };
 
-loadSavedButton.addEventListener("click", loadSavedNews);
+loadSavedButton.addEventListener("click", () => {
+  loadSavedNews();
+});
 loadNewsButton.addEventListener("click", () => {
   getNews(categorySelect.value);
 });
